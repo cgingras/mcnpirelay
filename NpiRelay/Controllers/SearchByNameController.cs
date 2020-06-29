950073a85813dd4fe799261f0297fe70e11c6d1f
Jeff Trotman <jeff@jefftrotman.com>
2020-06-29 15:50:47 -0500

    Add project files.

---
+++ NpiRelay/Controllers/SearchByNameController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Forcura.NPPES;
+using Forcura.NPPES.Models;
+namespace NpiRelay.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class SearchByNameController : ControllerBase
+    {
+		[HttpGet]
+		public async Task<IEnumerable<NPPESResult>> Get(string firstName, string lastName)
+		{
+			var response = await NPPESApiClient.SearchAsync(new NPPESRequest
+			{
+				FirstName = firstName,
+				LastName = lastName
+			});
+
+			return response.Results;
+		}
+
+	}
+}
---
+++ NpiRelay/Controllers/SearchByNpiNumberController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Forcura.NPPES;
+using Forcura.NPPES.Models;
+namespace NpiRelay.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class SearchByNpiNumberController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<IEnumerable<NPPESResult>> Get(string npiNumber)
+        {
+            var response = await NPPESApiClient.SearchAsync(new NPPESRequest
+            {
+                Number = npiNumber
+            });
+
+            return response.Results;
+        }
+    }
+}
